d46a5699873395462fb0e8ffd564cc8b6278ecf4
zsyOAOA <zsy20110806207@stu.xjtu.edu.cn>
2020-02-08 20:14:53 +0800

    Fix some bug

--- noise_estimation.py
+++ noise_estimation.py
@@ -5,8 +5,45 @@
 
 import numpy as np
 from cv2 import imread
-from utils import im2patch, im2double
+from skimage import img_as_float
 import time
+
+def im2patch(im, pch_size, stride=1):
+    '''
+    Transform image to patches.
+    Input:
+        im: 3 x H x W or 1 X H x W image, numpy format
+        pch_size: (int, int) tuple or integer
+        stride: (int, int) tuple or integer
+    '''
+    if isinstance(pch_size, tuple):
+        pch_H, pch_W = pch_size
+    elif isinstance(pch_size, int):
+        pch_H = pch_W = pch_size
+    else:
+        sys.exit('The input of pch_size must be a integer or a int tuple!')
+
+    if isinstance(stride, tuple):
+        stride_H, stride_W = stride
+    elif isinstance(stride, int):
+        stride_H = stride_W = stride
+    else:
+        sys.exit('The input of stride must be a integer or a int tuple!')
+
+
+    C, H, W = im.shape
+    num_H = len(range(0, H-pch_H+1, stride_H))
+    num_W = len(range(0, W-pch_W+1, stride_W))
+    num_pch = num_H * num_W
+    pch = np.zeros((C, pch_H*pch_W, num_pch), dtype=im.dtype)
+    kk = 0
+    for ii in range(pch_H):
+        for jj in range(pch_W):
+            temp = im[:, ii:H-pch_H+ii+1:stride_H, jj:W-pch_W+jj+1:stride_W]
+            pch[:, kk, :] = temp.reshape((C, num_pch))
+            kk += 1
+
+    return pch.reshape((C, pch_H, pch_W, num_pch))
 
 def noise_estimate(im, pch_size=8):
     '''
@@ -45,7 +82,7 @@ def noise_estimate(im, pch_size=8):
 
 if __name__ == '__main__':
     im = imread('./lena.png')
-    im = im2double(im)
+    im = img_as_float(im)
 
     noise_level = [5, 15, 20, 30, 40]
 
